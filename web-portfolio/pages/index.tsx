ba7301abcdcc2a6ba0b73d690751fb72689a3aef
safurutani <bluesara24@gmail.com>
2024-09-17 12:49:10 -0700

    restored font

--- web-portfolio/pages/index.tsx
+++ web-portfolio/pages/index.tsx
@@ -4,7 +4,7 @@ import Skills from '../components/Skills';
 const Home = () => (
   <div className="container mx-auto p-8 justify-center">
     <h1 className="text-4xl font-bold mb-2 text-center">Sara Furutani</h1>
-    <h2 className="text-2xl text-center mb-8">Self Taught Software Developer</h2>
+    <h2 className="text-2xl text-center mb-20">Self Taught Software Developer</h2>
     <Carousel />
     <Skills />
   </div>
